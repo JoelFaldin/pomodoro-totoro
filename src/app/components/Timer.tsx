50e00173469a1af2174df3ed04d7fcf261c0cd99
JoelFaldin <joelfaldin@gmail.com>
2024-09-29 18:33:50 -0300

    feat: adding icon and improving timer styles

--- src/app/components/Timer.tsx
+++ src/app/components/Timer.tsx
@@ -5,22 +5,26 @@ import { useEffect, useState } from "react"
 const Timer = () => {
   const [time, setTime] = useState(25 * 60)
   const [isRunning, setIsRunning] = useState(false)
+  const [isWork, setIsWork] = useState(true)
 
   useEffect(() => {
     let interval: NodeJS.Timeout | null = null
 
-    if (isRunning) {
+    if (isRunning && time > 0) {
       interval = setInterval(() => {
         setTime(prev => prev - 1)
       }, 1000)
+    } else if (time === 0) {
+      setIsRunning(false)
+      setIsWork(prev => !prev)
+      setTime(isWork ? 5 * 60 : 25 * 60)
     }
-    console.log(time)
 
     return () => {
       if (interval) clearInterval(interval)
     }
 
-  }, [isRunning, time])
+  }, [isRunning, time, isWork])
   
   const formatTime = (seconds: number) => {
     const mins = Math.floor(seconds / 60)
@@ -38,22 +42,32 @@ const Timer = () => {
     setIsRunning(false)
   }
 
+  const calculateProgress = () => {
+    const totalTime = isWork ? 25 * 60 : 5 * 60
+    return ((totalTime - time) / totalTime) * 100
+  }
+
   return (
-    <div className="bg-black rounded-lg shadow-md p-6 mb-8 w-2/5">
-      <h2 className="text-2xl font-semibold text-gray-200 mb-2">Pomodoro Timer</h2>
-      <p className="text-gray-300 mb-4">Use this timer to track your Pomodoro sessions</p>
+    <div className="bg-black rounded-lg shadow-md mb-8 w-3/12">
       <div className="flex flex-col items-center">
-        <div className="text-6xl font-bold mb-4" aria-live="polite">{formatTime(time)}</div>
-        <div className="flex space-x-4">
+        <h1 className="text-3xl font-bold text-center mb-8 w-full rounded-t-lg py-4 bg-gradient-to-r from-orange-500 to-orange-700">Pomodoro Timer</h1>
+        <div className="text-6xl text-slate-200 font-bold mb-4" aria-live="polite">{formatTime(time)}</div>
+        <div className="w-3/4 bg-gray-200 rounded-full h-2 my-5">
+          <div
+            className="bg-orange-500 h-2 rounded-full transition-all duration-300 ease-in-out"
+            style={{ width: `${calculateProgress()}%` }}
+          ></div>
+        </div>
+        <div className="flex space-x-4 py-5">
           <button
             onClick={toggleTimer}
-            className="w-24 py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 transition-colors"
+            className="w-24 py-2 px-4 bg-gray-500/70 text-white font-semibold rounded-lg shadow-md hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-400 focus:ring-opacity-75 transition-colors duration-200"
           >
             {isRunning ? 'Pause' : 'Start'}
           </button>
           <button
             onClick={resetTimer}
-            className="w-24 py-2 px-4 bg-gray-200 text-gray-700 font-semibold rounded-lg shadow-md hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-75 transition-colors"
+            className="w-24 py-2 px-4 bg-gray-200 text-gray-700 font-semibold rounded-lg shadow-md hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-75 transition-colors"
           >
             Reset
           </button>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" href="/totoro_icon.ico" sizes="any" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
